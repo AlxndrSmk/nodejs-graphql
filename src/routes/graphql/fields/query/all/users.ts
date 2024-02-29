37c6f7c0908cfa685a5d6975fbf4b2fdeb425dae
Alexander Samak <voprosy262@gmail.com>
2024-02-29 23:27:02 +0300

    fix: TS errors

--- src/routes/graphql/fields/query/all/users.ts
+++ src/routes/graphql/fields/query/all/users.ts
@@ -1,17 +1,19 @@
-import { GraphQLList, GraphQLResolveInfo } from 'graphql';
-import { user } from '../../../types/user.js';
-import { ContextValue } from '../../../types.js';
+import { GraphQLResolveInfo } from 'graphql';
 import {
   ResolveTree,
   parse,
   simplifyParsedResolveInfoFragmentWithType,
 } from 'graphql-parse-resolve-info';
 import DataLoader from 'dataloader';
-import { USERS_DL_KEY, USERS_DL_PRIME_KEY } from '../../../constants.js';
+import { ContextValue } from '../../../types/types.js';
 
 export const users = {
-  type: new GraphQLList(user),
-  async resolve(_source, _args, context: ContextValue, info: GraphQLResolveInfo) {
+  async resolve(
+    _source,
+    _args,
+    context: ContextValue,
+    info: GraphQLResolveInfo,
+  ): Promise<any> {
     const { fastify, dataLoaders } = context;
     const parsedResolveInfo = parse(info) as ResolveTree;
 
@@ -32,7 +34,7 @@ export const users = {
 
     const ids = users.map((user) => user.id);
 
-    let dl = dataLoaders.get(USERS_DL_KEY);
+    let dl = dataLoaders.get({ id: 'usersDl' } as const);
 
     if (!dl) {
       dl = new DataLoader(async (ids: readonly string[]) => {
@@ -42,10 +44,10 @@ export const users = {
 
         return sortedUsersInIdsOrder;
       });
-      dataLoaders.set(USERS_DL_KEY, dl);
+      dataLoaders.set({ id: 'usersDl' } as const, dl);
     }
 
-    dl.prime(USERS_DL_PRIME_KEY, users);
+    dl.prime('users', users);
 
     return dl.loadMany(ids);
   },
